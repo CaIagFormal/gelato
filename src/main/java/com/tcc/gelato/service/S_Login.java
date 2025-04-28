cd6ea5b2b27c2e14498dcd26d17dead1d4d3aa4b
CaIagFormal <carlos.tecnico.iagnecz@gmail.com>
2025-04-28 16:47:03 -0300

    Como que esqueci de enviar S_Login?!

---
+++ src/main/java/com/tcc/gelato/service/S_Login.java
@@ -0,0 +1,28 @@
+package com.tcc.gelato.service;
+
+import com.tcc.gelato.model.M_Usuario;
+import com.tcc.gelato.repository.R_Usuario;
+import org.springframework.stereotype.Service;
+
+/**
+ * Aplicação de regras de negócio do Login
+ */
+@Service
+public class S_Login {
+
+    private final R_Usuario r_usuario;
+
+    public S_Login(R_Usuario r_usuario) {
+        this.r_usuario = r_usuario;
+    }
+
+    /**
+     * Comunica com o repositório para obter um cadastro a partir dos parâmetros
+     * @param nome Nome ou E-mail do usuário
+     * @param senha Senha do usuário
+     * @return {@link com.tcc.gelato.model.M_Usuario} correspondente
+     */
+    public M_Usuario loginUsuario(String nome, String senha) {
+        return r_usuario.getUsuarioByNomeOrEmailAndSenha(nome,senha);
+    }
+}
