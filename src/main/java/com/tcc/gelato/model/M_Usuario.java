61fe1c6511ec6946c4c2f81ab5c42eadda96e54f
CaIagFormal <carlos.tecnico.iagnecz@gmail.com>
2025-04-28 14:14:31 -0300

    Getters/Setters para M_Usuario

--- src/main/java/com/tcc/gelato/model/M_Usuario.java
+++ src/main/java/com/tcc/gelato/model/M_Usuario.java
@@ -38,4 +38,52 @@ public class M_Usuario {
 
     @Column(nullable = false)
     private Cargo cargo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
 }
