52b41b5e55adf6f52b85bc80d06201fbb84de243
CaIagFormal <carlos.tecnico.iagnecz@gmail.com>
2025-05-12 17:17:35 -0300

    Conserto dos botões de produto

--- src/main/resources/static/js/inicio_cliente.js
+++ src/main/resources/static/js/inicio_cliente.js
@@ -1,5 +1,5 @@
 function abrir_produto() {
-    let id_produto = $($(this).find(".id_produto")[0]).text();
+    let id_produto = $($(this).find(".id_produto")[0]).text()
     $("form").attr("action","produto/"+id_produto);
     $("form").trigger("submit")
 }
