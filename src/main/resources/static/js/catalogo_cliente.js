a48e3e5e4e45e0e655ff84d58d5ffae0d617067d
CaIagFormal <carlos.tecnico.iagnecz@gmail.com>
2025-05-30 14:45:39 -0300

    Setores retraíveis no catálogo

--- src/main/resources/static/js/catalogo_cliente.js
+++ src/main/resources/static/js/catalogo_cliente.js
@@ -1,7 +1,19 @@
 function abrir_produto() {
-    let id_produto = $($(this).find(".id_produto")[0]).text()
+    let id_produto = $($(this).find(".id_produto")[0]).text();
     $("form").attr("action","produto/"+id_produto);
-    $("form").trigger("submit")
+    $("form").trigger("submit");
 }
 
-$(".btn_produto").click(abrir_produto)
+function abrir_setor() {
+    if ($(this).hasClass("aberto")) {
+        $(this).next(".setor").addClass("d-none");
+        $(this).removeClass("aberto");
+    } else {
+        $(this).next(".d-none.setor").removeClass("d-none");
+        $(this).addClass("aberto");
+    }
+}
+
+$(".btn_produto").click(abrir_produto);
+
+$(".btn_setor").click(abrir_setor);
