369ab4e5f98a3ea8afd619154e68334ae38b43b0
CaIagFormal <carlos.tecnico.iagnecz@gmail.com>
2025-08-18 17:23:27 -0300

    SecurityConfig e conserto do POM

--- src/main/java/com/tcc/gelato/security/SecurityConfig.java
+++ src/main/java/com/tcc/gelato/security/SecurityConfig.java
@@ -1,8 +1,50 @@
 package com.tcc.gelato.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+            //.formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("mario")
+                .password("armario")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("luigi")
+                .password("armario")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,user2);
+    }
 }
