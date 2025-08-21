f1d4897367c288d38d07cc2f1d2d641aa3923099
CarlosIagnecz <carlos.edu.iagnecz@gmail.com>
2025-08-20 23:05:53 -0300

    Correção de usuários cadastrados não serem redirecionados

--- src/main/java/com/tcc/gelato/config/SecurityConfig.java
+++ src/main/java/com/tcc/gelato/config/SecurityConfig.java
@@ -2,30 +2,22 @@ package com.tcc.gelato.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tcc.gelato.model.servidor.M_RespostaTexto;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
@@ -70,7 +62,8 @@ public class SecurityConfig {
             .sessionManagement(session ->
                     session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
             .addFilterBefore(f_jwt, UsernamePasswordAuthenticationFilter.class)
-            .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint()))
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint())
+                    .accessDeniedPage("/catalogo"))
             .authenticationProvider(authenticationProvider())
             .build();
     }
