cbef05ec7cc47f799be599ae2e10db368de2cb16
CarlosIagnecz <carlos.edu.iagnecz@gmail.com>
2025-08-21 15:54:53 -0300

    View da Observação TERMINADO

--- src/main/resources/static/js/observacao.js
+++ src/main/resources/static/js/observacao.js
@@ -1,3 +1,21 @@
 //Função de atualizar limite e conter texto dentro dele (1024 caracteres)
+function atualiza_texto() {
+    let texto = $('#obs-write').val();
+    let limite = $('#lim-obs');
+
+    if (texto.length>1023) {
+        $('#obs-write').val(texto.substring(0,1023));
+        limite.text('1023');
+        return;
+    }
+    limite.text(texto.length);
+}
+
+$('#obs-write').on('input',atualiza_texto)
 
 //Função de salvar observação
+function salvar_observacao() {
+    return;
+}
+
+$('#btn-salvar-obs').click(salvar_observacao)
