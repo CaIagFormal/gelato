69b0d56f482b49bad454a25044253182d8a99452
CaIagFormal <carlos.tecnico.iagnecz@gmail.com>
2025-08-25 14:40:31 -0300

    Novas tabelas do diagrama de entidades

---
+++ src/main/java/com/tcc/gelato/model/M_MetodoPagamento.java
@@ -0,0 +1,56 @@
+package com.tcc.gelato.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Representa um método de pagamento a um vendedor para ser listado em uma página
+ */
+@Entity
+@Table(name = "metodo_pagamento")
+public class M_MetodoPagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn( name = "fk_vendedor",nullable = false)
+    private M_Usuario vendedor;
+
+    @Column(nullable = false)
+    private String tipo;
+
+    @Column(nullable = false)
+    private String metodo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public M_Usuario getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(M_Usuario vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMetodo() {
+        return metodo;
+    }
+
+    public void setMetodo(String metodo) {
+        this.metodo = metodo;
+    }
+}
