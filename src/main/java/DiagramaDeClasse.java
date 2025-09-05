31d4b11f7ebdfc5b60266c12f17a11ee89524788
CaIagFormal <carlos.tecnico.iagnecz@gmail.com>
2025-09-05 17:12:51 -0300

    DiagramaDeClasse.java

---
+++ src/main/java/DiagramaDeClasse.java
@@ -0,0 +1,21 @@
+import com.tcc.gelato.config.*;
+import com.tcc.gelato.controller.*;
+import com.tcc.gelato.model.*;
+import com.tcc.gelato.repository.*;
+import com.tcc.gelato.service.*;
+import com.tcc.gelato.GelatoApplication;
+
+@Deprecated
+public class DiagramaDeClasse {
+    GelatoApplication gelatoApplication;
+
+    C_Cadastro c_cadastro;
+    C_Catalogo c_catalogo;
+    C_Error c_error;
+    C_Inicio c_inicio;
+    C_Login c_login;
+    C_Pedidos c_pedidos;
+    C_Produto c_produto;
+    C_Ticket c_ticket;
+    C_Transacao c_transacao;
+}
